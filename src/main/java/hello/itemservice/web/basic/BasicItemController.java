7fd8fe63f898939d36a1d3178746c1b9cf97f705
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-03-31 23:43:25 +0900

    commit

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -102,6 +102,35 @@ public class BasicItemController {
         // 대상 객체는 모델에 자동 등록된다. 나머지 사항은 기존과 동일하다.
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId,@ModelAttribute Item item){
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+
+    // 상품 수정은 상품 등록과 전체 프로세스가 유사하다.
+    //GET /items/{itemId}/edit : 상품 수정 폼
+    //POST /items/{itemId}/edit : 상품 수정 처리
+
+    //리다이렉트
+    //상품 수정은 마지막에 뷰 템플릿을 호출하는 대신에 상품 상세 화면으로 이동하도록 리다이렉트를 호출한다.
+    //스프링은 redirect:/... 으로 편리하게 리다이렉트를 지원한다.
+    //redirect:/basic/items/{itemId}
+    //컨트롤러에 매핑된 @PathVariable 의 값은 redirect 에도 사용 할 수 있다.
+    //redirect:/basic/items/{itemId} {itemId} 는 @PathVariable Long itemId 의 값을 그대로 사용한다.
+
+    //> 참고
+    //> HTML Form 전송은 PUT, PATCH를 지원하지 않는다. GET, POST만 사용할 수 있다.
+    //> PUT, PATCH는 HTTP API 전송시에 사용
+    //> 스프링에서 HTTP POST로 Form 요청할 때 히든 필드를 통해서 PUT, PATCH 매핑을 사용하는 방법이 있지만, HTTP 요청 상 POST 요청이다.
+
     // 테스트용 데이터 추가
     @PostConstruct
     public void init() {
