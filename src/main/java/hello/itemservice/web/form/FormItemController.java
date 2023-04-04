8826060303b42a93b985c4ca60ea6c9a4aaae7dd
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-04 14:59:59 +0900

    commit

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,8 +2,8 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -44,6 +45,19 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(Item item, RedirectAttributes redirectAttributes){
+        log.info("item.open={}", item.getOpen());
+        // item.open=true //체크 박스를 선택하는 경우
+        // item.open=false //체크 박스를 선택하지 않는 경우
+
+        // 체크 박스 체크
+        //open=on&_open=on
+        //체크 박스를 체크하면 스프링 MVC가 open 에 값이 있는 것을 확인하고 사용한다. 이때 _open 은 무시한다.
+
+        //체크 박스 미체크
+        //_open=on
+        //체크 박스를 체크하지 않으면 스프링 MVC가 _open 만 있는 것을 확인하고, open 의 값이 체크되지 않았다고 인식한다.
+        //이 경우 서버에서 Boolean 타입을 찍어보면 결과가 null 이 아니라 false 인 것을 확인할 수 있다.
+        //log.info("item.open={}", item.getOpen());
         Item saveItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId" , saveItem.getId());
         redirectAttributes.addAttribute("status" , true);
